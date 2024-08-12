3c610a58bdf3bf3f45ff3b905a664fa1cedcbad5
Sakilshikdar <sakilshikdar2504@gmil.com>
2024-08-12 09:09:44 +0600

    requirement-file-added

--- product_managment/urls.py
+++ product_managment/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls')),
+    path('api/', include('product.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
